f9b7f1fe3ef9cb03ec6072e07d8c30ee6eb249a2
aneylon <arlen.m.neylon@gmail.com>
2018-11-15 19:24:35 -0700

    Move Vue to second version document.

--- vueColors.js
+++ vueColors.js
@@ -1,8 +1,16 @@
 console.log('vue colors')
 
+Vue.component('color-swatch', {
+  props: ['color'],
+  template: '<li>{{color}}</li>'
+})
+
 var app = new Vue({
   el: '#colorApp',
   data: {
-    title: 'Random Color Scheme Generator'
+    title: 'Random Color Scheme Generator',
+    swatches:[
+      { id: 0, color: '#123456'}
+    ]
   }
 })
