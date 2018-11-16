3156029108a21ad818ed0d5fd0bf7be8248f25d5
Arlen Neylon <aneylon@scramsystems.com>
2018-11-16 08:32:06 -0700

    Setup for generating random color swatches.

--- vueColors.js
+++ vueColors.js
@@ -9,8 +9,18 @@ var app = new Vue({
   el: '#colorApp',
   data: {
     title: 'Random Color Scheme Generator',
+    numberOfColors: 3,
     swatches:[
       { id: 0, color: '#123456'}
     ]
+  },
+  methods: {
+    generateColor: function() {
+      console.log('make a new color!')
+    }
+  },
+  created: function() {
+    console.log('Start me up!')
+    this.generateColor()
   }
 })
