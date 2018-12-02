d3c4c5274fa00f917dee4b721b5670584d38aec3
Arlen Neylon <arlen.m.neylon@gmail.com>
2018-12-01 21:08:11 -0700

    Make first pass at rgb swatches.

--- vueColors.js
+++ vueColors.js
@@ -21,8 +21,9 @@ var app = new Vue({
     generateColor: function() {
       this.swatches = []
       for(let i = 0; i < this.numberOfColors; i++) {
-        let newNum = this.randomHSLNumber()
-        let newSwatch = this.createNewSwatch(newNum, i)
+        // let newNum = this.randomHSLNumber()
+        // let newSwatch = this.createNewSwatch(newNum, i)
+        let newSwatch = this.createRGBSwatch(i)
         this.swatches.push(newSwatch)
       }
     },
@@ -36,7 +37,11 @@ var app = new Vue({
       return Math.floor(Math.random() * max)
     },
     createNewSwatch: function(hslNumber, id) {
-      return { id, primaryColor: this.hslColorString(hslNumber), secondaryColor: this.hslColorComplimentString(hslNumber)}
+      return {
+        id,
+        primaryColor: this.hslColorString(hslNumber),
+        secondaryColor: this.hslColorComplimentString(hslNumber)
+      }
     },
     hslColorString: function (hslNumber) {
       return `hsl(${hslNumber}, ${this.randomHSLNumber(100)}%, ${this.randomNumber(100)}%)`
@@ -46,6 +51,34 @@ var app = new Vue({
         return `hsl(${hslNumber - 180}, 100%, 50%)`
       else
         return `hsl(${hslNumber + 180}, 100%, 50%)`
+    },
+    createRGBSwatch: function (id) {
+      let numbers = {
+        red: this.randomNumber(255),
+        green: this.randomNumber(255),
+        blue: this.randomNumber(255)
+      }
+      return {
+        id,
+        primaryColor: this.createRGBPrimary(numbers),
+        secondaryColor: this.createRGBCompliment(numbers)
+      }
+    },
+    createRGBPrimary: function (numbers) {
+      return `rgb(${numbers.red},${numbers.green},${numbers.blue})`
+    },
+    createRGBCompliment: function (numbers) {
+      console.log(numbers)
+      for(number in numbers) {
+        console.log(number, numbers[number])
+        let compliment = numbers[number] + 127
+        if(compliment > 255) {
+          compliment -= 255
+        }
+        numbers[number] = compliment
+      }
+      console.log(numbers)
+      return `rgb(${numbers.red},${numbers.green},${numbers.blue})`
     }
   },
   created: function() {
